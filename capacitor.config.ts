adc18184de2436338dcf7b8cfb58a4915cffe064
paulinhoraposo <paulinhoraposo@gmail.com>
2026-06-15 12:14:35 -0300

    alteração no .plist

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'central.vn3internet',
+  appId: 'com.vn3internet.app',
   appName: 'VN3 Internet',
   webDir: 'dist',
   plugins: {
